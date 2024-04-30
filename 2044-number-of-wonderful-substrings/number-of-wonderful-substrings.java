723abe78160155b10b051ac18265a6829e5a3d52
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-04-30 18:58:12 +0530

    Time: 19 ms (79.25%) | Memory: 44.9 MB (93.40%) - LeetSync

---
+++ 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long wonderfulSubstrings(String word) {
+        int[] count = new int[1024];
+        int mask = 0;
+        count[0] = 1;
+        long result = 0;
+        for(char ch: word.toCharArray()){
+            mask ^= 1 << (ch - 'a');
+            result += count[mask];
+            for(int i = 0; i < 10; i++){
+                result += count[mask^(1 << i)];
+            }
+            count[mask]++;
+        }
+
+        return result;
+    }
+}
