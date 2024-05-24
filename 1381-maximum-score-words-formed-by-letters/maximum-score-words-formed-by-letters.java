a5b7bac922e34e90a1608b0df147d205aa3ba35b
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-05-24 19:32:25 +0530

    Time: 1 ms (81.78%) | Memory: 41.8 MB (43.22%) - LeetSync

--- 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.java
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.java
@@ -1,48 +1,33 @@
 class Solution {
-
-    Map<Character, Integer> freq;
-    int[] scoree;
-
     public int maxScoreWords(String[] words, char[] letters, int[] score) {
-        scoree = score;
-        freq = new HashMap<>();
-        for(char c: letters){
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
-        }
+       int[] letterCounts = new int[26];
+   for (char letter : letters)
+       letterCounts[letter - 'a']++;
 
-        return backtrack(words, 0);
+   return backtrack(words, 0, letterCounts, score); 
     }
-    private int backtrack(String[] words, int idx){
-        if(idx == words.length)
-            return 0;
+    private int backtrack(String[] words, int start, int[] letterCounts, int[] score) {
+   if (start == words.length)
+       return 0;
 
-        int res = backtrack(words, idx + 1);
-        if(can_form_word(words[idx])){
-            for(char c: words[idx].toCharArray())
-                freq.put(c, freq.get(c) - 1);
+   int currScore = 0;
+   int wordScore = 0;
+   int[] nextCounts = letterCounts.clone();
+   for (int i = 0; i < words[start].length(); ++i) {
+       int idx = words[start].charAt(i) - 'a';
+       if (nextCounts[idx] == 0) {
+           wordScore = -1;
+           break;
+       }
+       wordScore += score[idx];
+       nextCounts[idx]--;
+   }
 
-            res = Math.max(res, get_score(words[idx]) + backtrack(words, idx + 1));
-            for(char c: words[idx].toCharArray())
-                freq.put(c, freq.get(c) + 1);    
-        } 
-        return res;   
-    }
-    private boolean can_form_word(String s){
-        Map<Character, Integer> map = new HashMap<>();
-        for(char c: s.toCharArray())
-            map.put(c, map.getOrDefault(c, 0) + 1);
+   if (wordScore > 0)
+       currScore = backtrack(words, start + 1, nextCounts, score) + wordScore;
+   currScore = Math.max(currScore, backtrack(words, start + 1, letterCounts, score));
 
-        for(char c: map.keySet()){
-            if(!freq.containsKey(c) || map.get(c) > freq.get(c))
-                return false;
-        }
-        return true;    
-    }
-    private int get_score(String s){
-        int score = 0;
-        for(char c: s.toCharArray()){
-            score += scoree[c - 'a'];
-        }
-        return score;
-    }
+   return currScore;
+
+}
 }
