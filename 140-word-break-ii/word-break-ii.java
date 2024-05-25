2c5759a87ccc98d7a32577590dedba5846e1b0b8
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-05-25 07:58:12 +0530

    Time: 1 ms (94.29%) | Memory: 41.8 MB (28.79%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,22 +1,35 @@
 class Solution {
-
-    List<String> ans;
-
-    private void solve(String s , List<String> dict,String str){
-        if(s.length()<=0){
-            ans.add(str.substring(1,str.length()));
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        List<List<String>> resultList = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        canBreak(0, 1, s, new HashSet<>(wordDict), resultList, new ArrayList<>());
+        for(List<String> temp : resultList) {
+            String joined = String.join(" ", temp);
+            // System.out.println(joined);
+            result.add(joined);
         }
-        for(int i = 0; i<s.length();i++){
-            if(dict.contains(s.substring(0,i+1))){
-                String temp = str+" "+s.substring(0,i+1);           
-                solve(s.substring(i+1,s.length()),dict,temp);
-            }
-        }
+        return result;
     }
 
-    public List<String> wordBreak(String s, List<String> wordDict) {
-        ans = new ArrayList<>();
-        solve(s,wordDict,"");
-        return ans;
+    void canBreak(int startIndex, int endIndex, String s, Set<String>  dict, List<List<String>> resultList, List<String> tempResult) {
+        // System.out.println("StartIdx : " + startIndex + ", endIndex : " + endIndex);
+        if(startIndex >= s.length()) {
+            resultList.add(new ArrayList<>(tempResult));
+            return;
+        }
+        if(endIndex > s.length()) {
+            return;
+        }
+        String subString = s.substring(startIndex, endIndex);
+        
+        if(dict.contains(subString)) {
+            // System.out.println("adding : " + subString);
+            tempResult.add(subString);
+            canBreak(endIndex, endIndex + 1, s, dict, resultList, tempResult);
+            // String lastEle = tempResult.get(tempResult.size() - 1);
+            // System.out.println("removing : " + lastEle);
+            tempResult.remove(tempResult.size() - 1);
+        }
+        canBreak(startIndex, endIndex + 1, s, dict, resultList, tempResult);
     }
 }
