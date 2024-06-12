26f1d5aa4b7f4ddaf43de640ba694c78c4b525d2
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-06-12 17:25:06 +0530

    Time: 2 ms (97.74%) | Memory: 44.4 MB (89.06%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,4 +1,5 @@
-
+/*  time complexity:O(n2)
+space :O(1)
 class Solution{
         public int[] twoSum(int[] nums, int target) {
                 for (int i=0;i<nums.length;i++){
@@ -14,27 +15,23 @@ class Solution{
         }
 
 }
+*/
 
-/*
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numMap = new HashMap<>();
-        int n = nums.length;
-
-        // Build the hash table
-        for (int i = 0; i < n; i++) {
-            numMap.put(nums[i], i);
+        int n=nums.length;
+        Map<Integer,Integer>map=new HashMap<>();
+      for(int i=0;i<n;i++){
+        int complement=target-nums[i];
+        if(map.containsKey(complement)){
+            return new int []{map.get(complement),i};
         }
+        //else
+        map.put(nums[i],i);
+      }
+      return null;
 
-        // Find the complement
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (numMap.containsKey(complement) && numMap.get(complement) != i) {
-                return new int[]{i, numMap.get(complement)};
-            }
-        }
 
-        return new int[]{}; // No solution found
     }
 }
-*/
