948004cd3ddbc16c14267a464a12ed05fd5ec3c9
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-06-12 09:36:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (60.77%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,8 +1,51 @@
-
-
+/* time complexity:O(nlogn)
+ space  complexity:O(1)
+inplace,stable:y,y
 class Solution {
     public int[] sortColors(int[] nums) {
         Arrays.sort(nums);
         return nums;
     }
 }
+*/
+//dutch national flag  algorithm
+//O(n),O(1)
+class Solution{
+      public void  sortColors(int[] nums) {
+        int low=0;int mid=0;int high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                //swap nums[low],nums[mid],increment low & mid
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                mid++;
+                low++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
+
+        }
+
+
+
+
+
+
+
+
+      }
+
+
+
+
+
+}
+
