e949b61f9062508fb21ddee36d22870bbc9df33c
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-06-12 16:56:08 +0530

    Time: 44 ms (39.68%) | Memory: 44.5 MB (89.06%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,40 @@
+
+class Solution{
+        public int[] twoSum(int[] nums, int target) {
+                for (int i=0;i<nums.length;i++){
+                    for (int j=i+1;j<nums.length;j++){
+                        if (nums[i]+nums[j]==target){
+                            return new int []{i,j};
+                        }
+                    }
+                }
+                return new int []{};
+
+
+        }
+
+}
+
+/*
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        // Build the hash table
+        for (int i = 0; i < n; i++) {
+            numMap.put(nums[i], i);
+        }
+
+        // Find the complement
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement) && numMap.get(complement) != i) {
+                return new int[]{i, numMap.get(complement)};
+            }
+        }
+
+        return new int[]{}; // No solution found
+    }
+}
+*/
