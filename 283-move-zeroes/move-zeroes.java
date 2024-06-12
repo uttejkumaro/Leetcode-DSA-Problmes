940f752be99e0ca20b0f0184531e71f05278039b
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-06-12 19:16:37 +0530

    Time: 2 ms (83.80%) | Memory: 46.3 MB (8.73%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,13 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int j=0;
-        for(int i=0;i<nums.length;i++){
-            
+        for(int i=0;i<nums.length;i++){            
             if(nums[i]!=0){
                 nums[j++]=nums[i];
             }
 
         }
+        //end of non zero elements add zeros at last by incrementing zeros
         while(j<nums.length){
             nums[j++]=0;
         }
