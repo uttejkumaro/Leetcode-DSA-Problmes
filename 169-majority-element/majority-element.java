84a4f4ca723fd1611ae6b70a4cbaccb70681b8a2
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-06-13 16:59:48 +0530

    Time: 2 ms (75.31%) | Memory: 53 MB (41.44%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,3 +1,4 @@
+/*
 //t.c:O(nlogn)
 class Solution {
     public int majorityElement(int[] nums) {
@@ -5,4 +6,23 @@ class Solution {
         return nums[nums.length/2];//middle element is the element which is present more than n/2
         
     }
+}
+*/
+class Solution{
+    public int  majorityElement(int[] nums) {
+        int c=0;
+        int v=0;
+        for(int num:nums){
+            if(v==0){
+                c=num;
+            }
+            if(c==num){
+                v++;
+            }
+            else{
+            v--;}
+        }
+        return c;
+    }
+
 }
