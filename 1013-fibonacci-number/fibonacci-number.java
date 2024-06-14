129fac5c598cfc423f5568c6a71edfcfe8604d1a
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-06-14 07:12:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (51.23%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int fib(int n) {
         if (n==0|| n==1){
@@ -7,5 +8,25 @@ class Solution {
        return fib(n-1)+fib(n-2);
             
         
+    }
+}
+*/
+class Solution {
+    public int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        
+        int a = 0;
+        int b = 1;
+        int c = 0; // Initial value
+        
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        
+        return c;
     }
 }
