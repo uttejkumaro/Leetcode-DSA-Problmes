55461182208fb17dadd5256e413437093dcaf4bf
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-06-17 08:04:45 +0530

    Time: 3 ms (97.79%) | Memory: 40.2 MB (65.09%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -1,12 +1,17 @@
-class Solution{
-public boolean judgeSquareSum(int c) {
-    int sqrtC = (int)Math.sqrt(c);
-    for (int i = 0; i <= sqrtC; i++) {
-        int j = (int)Math.sqrt(c - i * i);
-        if (i * i + j * j == c) {
-            return true;
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long a = (int)Math.sqrt(c); // truncate the decimal part
+        long b = 0;
+        while (b <= a) {
+            long sum = b * b + a * a;
+            if (sum == c) {
+                return true;
+            } else if (sum < c) {
+                b++;
+            } else {
+                a--;
+            }
         }
+        return false;
     }
-    return false;
-}
 }
