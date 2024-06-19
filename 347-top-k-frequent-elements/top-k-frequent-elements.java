20dff57d3dd4b306e7e634a9363219d8d9aab37a
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-06-19 20:26:08 +0530

    Time: 12 ms (80.09%) | Memory: 48.5 MB (53.08%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,4 +1,27 @@
+/*
+// 3 approaches
+1.Hashmap nlogn,o(n)
+2.heapify klogn
+3.Bucket sort (n),o(n)
 
+1. HashMap with Sorting
+Algorithm:
+Use a HashMap to count the frequency of each element.
+Convert the entry set of the HashMap to a list and sort it based on the frequency values.
+Extract the top K elements from the sorted list.
+Time Complexity: O(n log n) (due to sorting)
+Space Complexity: O(n)
+*/
+
+/* 
+2.2. Min-Heap
+Algorithm:
+Use a HashMap to count the frequency of each element.
+Use a Min-Heap to keep the top K elements based on their frequency.
+If the heap size exceeds K, remove the smallest frequency element.
+Extract elements from the heap.
+Time Complexity: O(n log k)
+Space Complexity: O(n)
 
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
@@ -20,3 +43,35 @@ class Solution {
         return res;
     }
 }
+*/
+//buket sort(Stroing array with counta as index  & map.entry represents  the key and value of map
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer>map=new HashMap<>();
+        for (int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+
+        // Initialize buckets array with length nums.length + 1
+        List<Integer> buckets[]=new List[nums.length+1];
+        for(int i=0;i<=nums.length;i++){
+            buckets[i]=new ArrayList<>();
+        }
+        for (Map.Entry<Integer,Integer>entry:map.entrySet()){
+            int freq=entry.getValue();
+            int num=entry.getKey();
+            buckets[freq].add(num);
+        }
+        //  Collect top K frequent elements
+        List<Integer> resultList = new ArrayList<>();
+        for (int i = buckets.length - 1; i >= 0 && resultList.size() < k; i--) {
+            if (!buckets[i].isEmpty()) {
+                resultList.addAll(buckets[i]);
+            }
+        }
+
+        //  Return the result as an array
+        return resultList.subList(0, k).stream().mapToInt(i -> i).toArray();
+    }
+}
+
