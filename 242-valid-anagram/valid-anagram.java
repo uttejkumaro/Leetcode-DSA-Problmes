09c321e80a0e182827affa242cd42bf13b1f5174
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-06-19 10:11:11 +0530

    Time: 6 ms (43.15%) | Memory: 43 MB (75.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -29,30 +29,7 @@ class Solution {
     }
 } 
 
-//using char count
-class Solution {
-    public boolean isAnagram(String s, String t) {
-    int n=s.length();
-    int m=t.length();
-    if (n!=m){
-        return false;
-    }
-    int count[]=new int[26];
-    for(int i=0;i<n;i++){
-        count[s.charAt(i)-'a']++;
-        count[t.charAt(i)-'a']--;
 
-    }
-    for(int i:count){
-        if (i!=0){
-            return false;
-        }
-    }
-    return true;
-    }  
-    
-}
-*/
 //using Hashmap
 class Solution {
     public boolean isAnagram(String s, String t) {
@@ -75,11 +52,35 @@ class Solution {
                 }
             }
         return true;
-    
-
-    
-
+        
    }
+}
+    */
+
+    
+
+//using char count
+class Solution {
+    public boolean isAnagram(String s, String t) {
+    int n=s.length();
+    int m=t.length();
+    if (n!=m){
+        return false;
+    }
+    int count[]=new int[26];
+    for(int i=0;i<n;i++){
+        count[s.charAt(i)-'a']++;
+        count[t.charAt(i)-'a']--;
+
+    }
+    for(int i:count){
+        if (i!=0){
+            return false;
+        }
+    }
+    return true;
+    }  
+    
 }
 
 
