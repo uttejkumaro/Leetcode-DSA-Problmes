0e51e838c7a6ae34ab4e889ef1a6b7549735ef61
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-06-20 20:57:44 +0530

    Time: 2 ms (89.87%) | Memory: 46.9 MB (70.13%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -12,6 +12,8 @@
     }
 }
 */
+/*
+//time complexity:O(n)
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         HashMap<Integer,Integer>map=new HashMap<>();
@@ -26,3 +28,24 @@ class Solution {
         return null;
     }
 }
+*/
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int l=0;
+        int r=numbers.length-1;
+        while(l<r){
+            if(numbers[l]+numbers[r]==target){
+                return new int[]{l+1,r+1};
+            }
+            else if(numbers[l]+numbers[r]>target){
+                r-=1;
+            }
+            else{
+                l+=1;
+            }
+        }
+        return null;
+
+    }
+}
+
