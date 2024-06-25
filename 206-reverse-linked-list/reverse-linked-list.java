844afd450e9bdfef23ab935fd20551a0821d80a8
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-06-25 07:12:45 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (83.79%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -7,7 +7,7 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- */
+ 
 class Solution {
     public ListNode reverseList(ListNode head) {
         //iteratevly
@@ -23,5 +23,18 @@ class Solution {
         head=prev;
         return head;
         
+    }
+}
+*/
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head==null || head.next==null){
+            return head;
+        }
+         ListNode reversedList = reverseList(head.next);
+        head.next.next=head;
+        head.next=null;
+        return  reversedList;
+
     }
 }
