935fa3c9cfcd9c15a13c6f2fe7d8c3691b0a83de
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-06-27 08:36:15 +0530

    Time: 0 ms (100.00%) | Memory: 67.2 MB (46.90%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -4,12 +4,21 @@ class Solution {
         int node2=edges[0][1];
         int node3 =edges[1][0];
         int node4 =edges[1][1];
+        /*
         if(node1==node3 || node1==node4){
             return node1;
         }
         else{
             return node2;
         }
+        */
+        if(node2==node3 || node2==node4){
+            return node2;
+        }
+        else{
+            return node1;
+        }
+
 
         
     }
