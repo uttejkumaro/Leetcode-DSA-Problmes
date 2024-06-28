e4d797c1475b8a59bb45307b6380900d817daebc
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-06-28 10:29:08 +0530

    Time: 1 ms (66.63%) | Memory: 41.6 MB (91.39%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            TreeNode rightMost = null;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                rightMost = curr;
+
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+            }
+
+            // After the loop, rightMost will be the last node of the current level
+            if (rightMost != null) {
+                res.add(rightMost.val);
+            }
+        }
+
+        return res;
+    }
+}
