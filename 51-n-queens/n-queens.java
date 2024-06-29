597ce39df115c0e7cbb06bd22d388326bfb2bc42
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-06-29 18:07:39 +0530

    Time: 2 ms (88.40%) | Memory: 44.8 MB (30.67%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,64 @@
+public class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> sol = new ArrayList<>();
+        char[][] board = new char[n][n];
+        
+        // Initialize the board with '.'
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        
+        // Call the backtracking function
+        backtrack(sol, board, 0);
+        return sol;
+    }
+
+    private void backtrack(List<List<String>> sol, char[][] board, int row) {
+        if (row == board.length) {
+            sol.add(construct(board));
+            return;
+        }
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                backtrack(sol, board, row + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int row, int col) {
+        // Check the column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check the 45° diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check the 135° diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private List<String> construct(char[][] board) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            res.add(new String(board[i]));
+        }
+        return res;
+    }
+}
