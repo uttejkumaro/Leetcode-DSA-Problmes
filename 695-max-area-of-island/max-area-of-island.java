0ebc91536e855e2f6a4e43678bd892ae09682a4b
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-07-01 10:52:27 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (91.17%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.java
@@ -0,0 +1,24 @@
+class Solution {
+    int maxArea=0;
+    public int maxAreaOfIsland(int[][] grid) {
+      
+        for( int i=0;i<grid.length;i++){
+            for(int  j=0;j<grid[0].length;j++){
+                maxArea=Math.max(maxArea,maxAreaIsland(grid,i,j));
+            }
+        }
+        return maxArea;        
+    }
+    int  maxAreaIsland(int [][]grid,int r,int c){
+        if(r<0 || c<0 || r==grid.length || c==grid[0].length || grid[r][c]==0 ){
+            return 0;
+        }
+        grid[r][c]=0;
+        return(1+maxAreaIsland(grid,r+1,c)+
+                maxAreaIsland(grid,r-1,c)+
+                maxAreaIsland(grid,r,c+1)+
+                maxAreaIsland(grid,r,c-1) );
+                
+    }
+    
+}
