18249988bdf4a86f5f778094c51105062253ea09
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-07-01 09:15:58 +0530

    Time: 64 ms (26.15%) | Memory: 61.1 MB (26.70%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -5,17 +5,34 @@
     }
 }*/
 //we can use minheap to find largest by removing less than thta largerst elemrnt
+
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> minHeap=new PriorityQueue<>();
-        for(int i:nums){
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int i : nums) {
             minHeap.add(i);
-        
-        if(minHeap.size()>k){
-            minHeap.poll();
+             // Print the entire minHeap
+        //System.out.println("Contents of the minHeap:");
+        //for (int num : minHeap) {
+           // System.out.println(num);
+        //}
+            if (minHeap.size() > k) {
+                minHeap.poll();
             }
         }
-        return minHeap.peek();
+        
+       
 
-    } 
+        return minHeap.peek();
+    }
+    /*
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+        int result = sol.findKthLargest(nums, k);
+        System.out.println("The " + k + "th largest element is: " + result);
+    }
+    */
 }
