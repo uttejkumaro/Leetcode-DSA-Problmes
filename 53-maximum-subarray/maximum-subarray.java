d0a7c8d71a840966affdca0d3656787be24466b0
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-07-02 07:07:07 +0530

    Time: 1 ms (99.55%) | Memory: 57.2 MB (23.16%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,9 +1,11 @@
+
 class Solution {
     public int maxSubArray(int[] nums) {
         //kadanes algo is sum<o then sum=0;
         int maxi=nums[0],sum=0;
         for(int i=0;i<nums.length;i++){
             sum=sum+nums[i];
+            //max=math.max(sum,maxi)
             if(sum>maxi){
                 maxi=sum;
             }
@@ -11,7 +13,7 @@ class Solution {
                 sum=0;
             }
         }
-        return maxi;        
+        return maxi;      
         
     }
 }
