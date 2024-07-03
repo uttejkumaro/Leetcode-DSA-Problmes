29238b3b655f25ceb8d9e2074971d9d016a53816
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-07-03 14:54:54 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (91.51%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
         if(n==1){
@@ -22,4 +22,41 @@ class Solution {
         return dp[dp.length-1];
 
     }
+}
+*/
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        if(n==1){
+            return nums[0];
+            }
+              int lSkip[]=new int [n];
+              int fSkip[]=new int [n];
+            for(int i=0;i<n-1;i++){
+                 lSkip[i]=nums[i];
+                 fSkip[i]=nums[i+1];
+            }
+            int lootLskip=helper(lSkip);
+            int lootFskip=helper(fSkip);
+            return Math.max(lootLskip,lootFskip);
+            
+    }
+      public int helper(int[] nums) {
+        int n=nums.length;
+        if(n==1){
+            return nums[0];
+        }
+        int []dp=new int[n];
+        //memoize firts two values
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);//imp logic
+
+        }
+        return dp[n-1];
+        
+    }
+
+
 }
