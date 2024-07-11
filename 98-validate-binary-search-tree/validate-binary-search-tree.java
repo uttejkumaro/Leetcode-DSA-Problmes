c4dbd90079b697357aaaed364a97f1fd58b020ff
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-07-11 16:37:53 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (16.92%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return dfs(root, null, null); // Start DFS traversal from the root node with no initial range constraints
+    }
+   boolean dfs(TreeNode node, Integer min, Integer max) {
+        // Base case: null node is a valid BST
+        if (node == null) {
+            return true;
+        }
+
+        // Check if current node's value violates the BST property
+        if ((min != null && node.val <= min) || (max != null && node.val >= max)) {
+            return false;
+        }
+
+        // Recursively check left and right subtrees with updated range constraints
+        return dfs(node.left, min, node.val) && dfs(node.right, node.val, max);
+    }
+}
