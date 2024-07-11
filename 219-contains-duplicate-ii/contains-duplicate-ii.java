2f37831099b3af3143baaa19c4679423ff3a1e9e
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-07-11 15:26:18 +0530

    Time: 18 ms (70.18%) | Memory: 57.2 MB (50.66%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -31,3 +31,19 @@ class Solution {
         
     }
 }
+/*
+ class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                int v = map.get(nums[i]);
+                if(Math.abs(v-i)<=k){
+                    return true;
+                }
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+} /*/
