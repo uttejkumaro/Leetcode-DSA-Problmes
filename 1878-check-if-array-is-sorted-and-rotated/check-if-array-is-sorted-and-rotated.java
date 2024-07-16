1494a51ae2634eb0662b0ed32db6c1baad433ce1
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-07-16 17:41:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (7.15%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -3,11 +3,11 @@ class Solution {
         int count=0;
         for(int i=1; i<nums.length; i++){
             if(nums[i-1]>nums[i]){
-                count++;
+                count++;//only one element satissfies whwere it is broken
             }
         }
         if(nums[nums.length-1]>nums[0]){
-            count++;
+            count++;//fora sortd array
         }
         if(count>1){
             return false;
