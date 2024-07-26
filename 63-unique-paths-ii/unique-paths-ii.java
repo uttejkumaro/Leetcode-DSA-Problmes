87c2ac52f0d065513444fb00b15157ba09577354
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-07-26 21:19:11 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (56.44%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -20,6 +20,7 @@ class Solution {
     }
 }
 */
+/* 
 // tabulation
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
@@ -54,5 +55,37 @@ class Solution {
             }
         }  
         return dp[n-1][m-1];    
+    }
+}
+*/
+//space optimisation
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+        int[] prev = new int[m];
+        
+        for (int i = 0; i < n; i++) {
+            int[] curr = new int[m];
+            for (int j = 0; j < m; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    curr[j] = 0;
+                } else if (i == 0 && j == 0) {
+                    curr[j] = 1;
+                } else {
+                    int up = 0, left = 0;
+                    if (i > 0) {
+                        up = prev[j];
+                    }
+                    if (j > 0) {
+                        left = curr[j - 1];
+                    }
+                    curr[j] = up + left;
+                }
+            }
+            prev = curr;
+        }   
+        
+        return prev[m - 1];     
     }
 }
