f8622f9d423fb91c8513cff7052381791e4991b2
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-07-27 20:41:03 +0530

    Time: 4 ms (48.50%) | Memory: 44.5 MB (18.15%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,3 +1,4 @@
+/* //memoization 
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n=triangle.size();
@@ -18,3 +19,38 @@ class Solution {
 
     }
 }
+/*
+first iterartion
+[ [-1, -1, -1, -1],
+  [-1, -1, -1, -1],
+  [-1, -1, -1, -1],
+  [ 8,  9,  6, 10]
+]
+dp = [  [14, -1, -1, -1],
+  [13, 15, -1, -1],
+  [11, 12, 13, -1],
+  [ 8,  9,  6, 10]
+]
+*/
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+          int n=triangle.size();
+          int dp[][]=new int[n][n];
+          for(int j=0;j<n;j++){
+            dp[n-1][j]=triangle.get(n-1).get(j);
+          }
+          for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down =triangle.get(i).get(j)+dp[i+1][j];
+                int diagonal =triangle.get(i).get(j)+dp[i+1][j+1];
+                dp[i][j]=Math.min(down,diagonal);
+
+            }
+          }
+          return dp[0][0];       
+
+
+    }
+}
+        
+        
