63ba6d23fba171704d35f127c7adc54b9c2fcd33
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-07-30 15:01:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (46.63%) - LeetSync

--- 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.java
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.java
@@ -1,9 +1,10 @@
+/*
 class Solution {
     public boolean checkString(String s) {
         boolean flag=false;
         for(char c:s.toCharArray()){
             if(c=='a'){
-                if(flag){
+                if(flag){ //tells that a is presnt after b
                     return false;
                 }
             }
@@ -11,6 +12,16 @@ class Solution {
                 flag=true;
             }
 
+        }
+        return true;
+    }
+}
+*/
+//2nd way 
+class Solution {
+    public boolean checkString(String s) {
+        for(int i=0;i<s.length()-1;i++){
+            if(s.charAt(i)=='b' && s.charAt(i+1)=='a') return false;
         }
         return true;
     }
