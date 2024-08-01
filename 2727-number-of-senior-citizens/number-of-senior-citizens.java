2464f263e543bb9d31f1ad0f65e5c2075364d1a8
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-08-01 06:56:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (45.62%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int countSeniors(String[] details) {
         //StringBUilder sb=new StringBUilder();
@@ -7,5 +8,19 @@ class Solution {
        if(age>60)count++;
       }
         return count;
+    }
+}
+*/
+class Solution {
+    public int countSeniors(String[] details) {
+        int count=0;
+        for(String detail:details){
+            int  tens=detail.charAt(11)-'0';
+            int  ones=detail.charAt(12)-'0';
+            int age =tens*10+ones;
+            if(age>60)count++;
+                 }
+        return count;          
+        
     }
 }
