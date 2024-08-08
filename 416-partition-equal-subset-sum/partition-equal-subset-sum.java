8f20c7461dbf85740a0ab225fc910eba98b4eee1
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-08-08 14:44:36 +0530

    Time: 27 ms (80.91%) | Memory: 42 MB (90.32%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -33,8 +33,37 @@
 //     }
 // }
  //tabulation
-class Solution {
+// class Solution {
    
+//     public boolean canPartition(int[] nums) {
+//         int n=nums.length;
+//         int sum=0;
+//         for(int i:nums){
+//             sum+=i;
+//         }
+//         if(sum%2!=0){
+//             return false;
+//         }
+//         int target=sum/2;
+//         boolean dp[][]=new boolean[nums.length+1][target+1];;
+//         for(int i=0;i<nums.length;i++){
+//             dp[i][0]=true;
+//         }
+//         for(int i=1;i<=nums.length;i++){
+//             for(int j=1;j<=target;j++){
+//                 if(nums[i-1]<=j){
+//                     dp[i][j]=dp[i-1][j] || dp[i-1][j-nums[i-1]];
+//                 }
+//                 else{
+//                     dp[i][j]=dp[i-1][j];
+//                 }
+//             }
+//         }
+//         return dp[nums.length][target];
+//     }
+// }
+//space optimisation 
+class Solution {   
     public boolean canPartition(int[] nums) {
         int n=nums.length;
         int sum=0;
@@ -45,20 +74,15 @@ class Solution {
             return false;
         }
         int target=sum/2;
-        boolean dp[][]=new boolean[nums.length+1][target+1];;
+        boolean dp[]=new boolean[target+1];;
+            dp[0]=true;
+        
         for(int i=0;i<nums.length;i++){
-            dp[i][0]=true;
-        }
-        for(int i=1;i<=nums.length;i++){
-            for(int j=1;j<=target;j++){
-                if(nums[i-1]<=j){
-                    dp[i][j]=dp[i-1][j] || dp[i-1][j-nums[i-1]];
-                }
-                else{
-                    dp[i][j]=dp[i-1][j];
-                }
+            for(int j=target;j>=nums[i];j--){
+                dp[j]=dp[j] || dp[j-nums[i]];
+                
             }
         }
-        return dp[nums.length][target];
+        return dp[target];
     }
 }
