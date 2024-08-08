636e92023a06f2703654aad2d56ea3085e676452
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-08-08 14:31:01 +0530

    Time: 86 ms (27.43%) | Memory: 57.3 MB (21.72%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,23 +1,33 @@
 class Solution {
+    private  boolean canPartition(int nums[],int target,int index,Boolean memo[][]){
+        if(target==0){
+            return  true;
+        }
+        if(index<0 ||target<0){
+            return false;
+        }
+        if(memo[index][target]!=null){
+            return memo[index][target];
+        }
+        boolean include = canPartition(nums,target-nums[index],index-1,memo);
+        boolean exclude = canPartition(nums,target,index-1,memo);
+        memo[index][target]=include || exclude;
+        return memo[index][target];
+
+
+    }
     public boolean canPartition(int[] nums) {
         int n=nums.length;
         int sum=0;
-        for(int num:nums){
-            sum+=num;
+        for(int i:nums){
+            sum+=i;
         }
         if(sum%2!=0){
             return false;
         }
-        int target =sum/2;
-       boolean dp[]=new boolean[target+1];
-       dp[0]=true;
-       for(int num:nums){
-        for(int i=target;i>=num;i--){
-            dp[i]=dp[i] || dp[i-num];
-
-        }
-       }
-       return dp[target];
-
+        int target=sum/2;
+        Boolean memo[][]=new Boolean[nums.length][target+1];;
+        return canPartition(nums,target,nums.length-1,memo); 
+        
     }
 }
