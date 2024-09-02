4a9598d389c6b1a5f0ab6811b7affc0ae1cdfa0b
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-09-02 07:17:17 +0530

    Time: 2 ms (72.59%) | Memory: 60.5 MB (8.89%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
@@ -9,6 +9,7 @@ class Solution {
         //     while(s<k) s=k-s;
         // }
         //for remaining use modulo
+       // k=k%s //;oose conerstion java wont hadel tpe cast if we implict sppecify so error therow
          k%=s;
         for(int i=0;i<n;i++){
             if(chalk[i]>k){
