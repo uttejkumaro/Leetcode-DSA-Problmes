ff8b6f7181dacda2655a37535a2352f210fe2e65
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-09-19 19:23:16 +0530

    Time: 25 ms (35.83%) | Memory: 44.7 MB (91.14%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.java
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -9,6 +9,7 @@ class Solution {
        for(int i:mp.keySet()){
         if(i%2==0){
            int  freq=mp.get(i);
+           //upadte if curr freq is greater than maxfreq  or equal freq update small num;
             if(freq>maxFreq || (freq==maxFreq && i<ele)){
                 ele=i;
                 maxFreq=freq;
