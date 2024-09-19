5dbd61bbc7a469c598a4a9224499b7a005e50318
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-09-19 19:21:56 +0530

    Time: 25 ms (35.83%) | Memory: 45.2 MB (18.19%) - LeetSync

---
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+       HashMap<Integer,Integer>mp=new HashMap<>();
+       int ele=-1;
+       int maxFreq=0;
+       for(int i:nums){
+        mp.put(i,mp.getOrDefault(i,0)+1);
+       }
+       for(int i:mp.keySet()){
+        if(i%2==0){
+           int  freq=mp.get(i);
+            if(freq>maxFreq || (freq==maxFreq && i<ele)){
+                ele=i;
+                maxFreq=freq;
+            }
+        }
+        
+       }
+       return ele;
+        
+    }
+}
