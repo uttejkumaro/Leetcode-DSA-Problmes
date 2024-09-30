d5d820b24517766179b1d5f06aa7080ec31f9141
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-09-30 17:14:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (59.05%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,43 +1,37 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int idx=-1;
         int n=nums.length;
-        for(int i=n-1;i>=1;i--){
-            if(nums[i]>nums[i-1]){
-                idx=i-1;
+        int idx=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx=i;
+                break;
+                
+            }
+        }
+        if(idx==-1) {
+            reverse(nums,0,n-1);
+            return;
+        }
+        //swap n-2 with i
+        for(int i=n-1;i>idx;i--){
+            if(nums[i]>nums[idx]){
+                int temp=nums[i];
+                nums[i]=nums[idx];
+                nums[idx]=temp;
                 break;
             }
         }
-        if(idx==-1){
-            int i=0,j=n-1;
-            while(i<j){
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-                i++;
-                j--;
-            }
-            return;
+        reverse(nums,idx+1,n-1);
+        
+    }
+    void reverse(int nums[],int st,int end){
+        while(st<end){
+            int temp=nums[st];
+            nums[st]=nums[end];
+            nums[end]=temp;
+            st++;
+            end--;
         }
-        else{
-            for(int i=n-1;i>idx;i--){
-                if(nums[i]>nums[idx]){
-                    int temp =nums[idx];
-                    nums[idx]=nums[i];
-                    nums[i]=temp;
-                    break;
-
-                }
-            }
-        }
-        int i=idx+1,j=n-1;
-        while(i<j){
-            int temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
-            i++;
-            j--;
-        }
-        return;
     }
 }
