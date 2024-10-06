646e79eeb108f93c8b375afe8a06c950a643d4a4
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-10-06 14:52:33 +0530

    Time: 3 ms (15.48%) | Memory: 41.8 MB (24.60%) - LeetSync

---
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        Deque<String>dq1=new ArrayDeque<>(Arrays.asList(sentence1.split(" ")));
+        Deque<String>dq2=new ArrayDeque<>(Arrays.asList(sentence2.split(" ")));
+        //compare and remove common prefix
+        while(!dq1.isEmpty() && !dq2.isEmpty() && dq1.peek().equals(dq2.peek())){
+            dq1.poll();
+            dq2.poll();
+        }
+        //compare and remnove common suffix
+        while(!dq1.isEmpty() && !dq2.isEmpty() && dq1.peekLast().equals(dq2.peekLast())){
+                dq1.pollLast();
+                dq2.pollLast();
+        }
+        return dq1.isEmpty()||dq2.isEmpty();
+        
+    }
+}
