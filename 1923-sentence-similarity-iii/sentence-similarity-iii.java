5f2d511f9c69b66f9eda41586664716636f0dac3
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-10-06 18:57:22 +0530

    Time: 2 ms (22.62%) | Memory: 41.2 MB (96.83%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.java
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.java
@@ -1,9 +1,22 @@
 class Solution {
     public boolean areSentencesSimilar(String sentence1, String sentence2) {
-        Deque<String>dq1=new ArrayDeque<>(Arrays.asList(sentence1.split(" ")));
-        Deque<String>dq2=new ArrayDeque<>(Arrays.asList(sentence2.split(" ")));
+        // Deque<String>dq1=new ArrayDeque<>(Arrays.asList(sentence1.split(" ")));
+        // Deque<String>dq2=new ArrayDeque<>(Arrays.asList(sentence2.split(" ")));
         //compare and remove common prefix
-        while(!dq1.isEmpty() && !dq2.isEmpty() && dq1.peek().equals(dq2.peek())){
+    
+        // array deque accepts only collections as input 
+        Deque<String> dq1 = new ArrayDeque<>();
+        Deque<String> dq2 = new ArrayDeque<>();
+
+    // Adding elements from the array manually
+    for(String word:sentence1.split(" ")){
+        dq1.add(word);
+    }
+    for(String word:sentence2.split(" ")){
+        dq2.add(word);
+}
+
+        while(!dq1.isEmpty()&&!dq2.isEmpty()&& dq1.peek().equals(dq2.peek())){
             dq1.poll();
             dq2.poll();
         }
