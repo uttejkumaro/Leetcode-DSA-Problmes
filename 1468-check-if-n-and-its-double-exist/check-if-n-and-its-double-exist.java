8b020d79c7383f01cde2dcb5b4ee703173dd73bd
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-12-01 15:10:00 +0530

    Time: 2 ms (93.52%) | Memory: 43.2 MB (26.42%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,14 +1,28 @@
-class Solution {
-    public boolean checkIfExist(int[] arr) {
-        int n=arr.length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(arr[i]==arr[j]*2 && i!=j){
-                    return true;
-                }
-            }
-        }
-        return false;
+// class Solution {
+//     public boolean checkIfExist(int[] arr) {
+//         int n=arr.length;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 if(arr[i]==arr[j]*2 && i!=j){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
         
+//     }
+// }
+ class Solution {
+  public boolean checkIfExist(int[] arr) {
+    int n=arr.length;
+    HashMap<Integer,Integer>mp=new HashMap<>();
+    for(int i=0;i<n;i++){
+        int num=arr[i];
+        if(mp.containsKey(2*num) || ( num%2 ==0  &&mp.containsKey(num/2) )){
+            return true;
+        }
+        mp.put(num,mp.getOrDefault(num,0)+1);
     }
-}
+    return false;
+  }
+ }
