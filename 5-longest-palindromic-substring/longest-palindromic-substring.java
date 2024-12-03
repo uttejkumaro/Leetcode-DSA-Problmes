e2f48585c36a534011958a7ba306cf470610f40b
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-12-03 16:50:44 +0530

    Time: 1573 ms (8.85%) | Memory: 42.2 MB (80.34%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,85 +1,37 @@
-class Solution {
-    public String longestPalindrome(String s) {
-        int n = s.length();
-        
-        if (n <= 1) {
-            return s;
-        }
+import java.io.*;
+import java.util.*;
 
-        String lps = "";
-        
-        // Odd length palindromes
-        for (int i = 0; i < n; i++) {
-            int l = i;
-            int r = i;
-            
-            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
-                l--;
-                r++;
-            }
-            
-            String palindrome = s.substring(l + 1, r);
-            if (palindrome.length() > lps.length()) {
-                lps = palindrome;
-            }
-        }
-        
-        // Even length palindromes
-        for (int i = 0; i < n; i++) {
-            int l = i - 1;
-            int r = i;
-            
-            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
-                l--;
-                r++;
-            }
-            
-            String palindrome = s.substring(l + 1, r);
-            if (palindrome.length() > lps.length()) {
-                lps = palindrome;
-            }
-        }
-        
-        return lps;
-    }
-}
-
-/*
-class Solution {
-    public String longestPalindrome(String s) {
+public class Solution {
+    static String longestPalindrome(String s){
         int n=s.length();
-    if (n == 1) {
-        return s;
-    }
-    
-    String lps = "";
-    
-    // Odd-length palindromes
-    for (int i = 0; i < n; i++) {
-        int left = i, right = i;
-        while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
-            if (right - left + 1 > lps.length()) {
-                lps = s.substring(left, right + 1);
+        int count=1;
+        int start=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                
+                if(ispali(s,i,j) && j-i+1>count){
+                    start=i;
+                    count=j-i+1;
+                }
             }
-            left--;
-            right++;
+            
         }
+        return s.substring(start,start+count);
     }
-    
-    // Even-length palindromes
-    for (int i = 0; i < n - 1; i++) {
-        int left = i, right = i + 1;
-        while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
-            if (right - left + 1 > lps.length()) {
-                lps = s.substring(left, right + 1);
-            }
-            left--;
-            right++;
-        }
+    static boolean ispali(String s,int i,int j){
+       while(i<j){
+           if(s.charAt(i)!=s.charAt(j)) return false;
+           i++;
+           j--;
+       }
+        
+        return true;
     }
-    
-    return lps;
-}
-}
-*/
 
+    // public static void main(String[] args) {
+    //    Scanner sc=new Scanner(System.in);
+    //     String s=sc.next();
+    //     System.out.println(pali(s));
+        
+    // }
+}
