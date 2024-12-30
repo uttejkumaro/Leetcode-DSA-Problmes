c96e392b9e0bf29109af7d47a9da32ea9a1ff487
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2024-12-30 19:28:25 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (40.84%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,10 +1,38 @@
+// class Solution {
+//     public boolean search(int[] nums, int target) {
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]==target){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
 class Solution {
-    public boolean search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                return true;
+   public boolean search(int[] nums, int target) {
+    int low=0,high=nums.length-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target) return true;
+        if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+            low++;
+            high--;
+            continue;
+        }
+        if(nums[low]<=nums[mid]){
+            if(nums[low]<=target && target<=nums[mid]) high=mid-1;
+            else low=mid+1;
+        }
+            else {
+                if(nums[mid]<=target && target<=nums[high]) low=mid+1;
+                else high=mid-1;
             }
         }
-        return false;
-    }
+
+    
+    return false;
+    
+
+   
+   }
 }
