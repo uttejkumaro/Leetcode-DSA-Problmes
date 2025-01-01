e12e48763ebb9d5963e19475380a9d4be0ed90dd
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-01-01 16:46:19 +0530

    Time: 1 ms (97.36%) | Memory: 41.7 MB (25.32%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -1,24 +1,40 @@
+// class Solution {
+//     public int maxScore(String s) {
+//         int n=s.length();
+//         int res=0;
+//         for(int  i=0;i<n-1;i++){
+//             int ans=0;
+//             for(int j=0;j<i+1;j++){
+//                 if(s.charAt(j)=='0'){
+//                     ans++;
+
+//                 }
+//             }
+//             for(int j=i+1;j<n;j++){
+//                 if(s.charAt(j)=='1'){
+//                     ans++;
+//                 }
+//             }
+//             res=Math.max(ans,res);
+//         }
+//         return res;        
+//     }
+// }
 class Solution {
     public int maxScore(String s) {
-        int n=s.length();
-        int res=0;
-        for(int  i=0;i<n-1;i++){
-            int ans=0;
-            for(int j=0;j<i+1;j++){
-                if(s.charAt(j)=='0'){
-                    ans++;
+         int n=s.length();
+        int ones=0,zeros=0,res=0;
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='1') ones++;
+        }
+        //check if left part one is there dec 1 or ince zero count
+        for(int i=0;i<n-1;i++){
+            if(s.charAt(i)=='1') ones--;
+            else zeros++;
+            res=Math.max(res,zeros+ones);
 
-                }
-            }
-            for(int j=i+1;j<n;j++){
-                if(s.charAt(j)=='1'){
-                    ans++;
-                }
-            }
-            res=Math.max(ans,res);
         }
         return res;
 
-        
     }
 }
