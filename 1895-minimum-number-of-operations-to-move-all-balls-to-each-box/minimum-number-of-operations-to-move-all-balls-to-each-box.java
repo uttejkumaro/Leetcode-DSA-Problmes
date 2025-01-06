c36f84c96c66dadefd50f821ea94d3007c5439d5
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-01-06 20:44:59 +0530

    Time: 4 ms (76.56%) | Memory: 44.8 MB (65.63%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,19 +1,45 @@
-class Solution {
-    public int[] minOperations(String boxes) {
-        int n=boxes.length();
-        int res[]=new int[boxes.length()];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+// class Solution {
+//     public int[] minOperations(String boxes) {
+//         int n=boxes.length();
+//         int res[]=new int[boxes.length()];
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
                 
-                    if(boxes.charAt(j)=='1'){
-                        res[i]+=Math.abs(i-j);
+//                     if(boxes.charAt(j)=='1'){
+//                         res[i]+=Math.abs(i-j);
 
                     
-                }
-            }
-        }
-        return res;
+//                 }
+//             }
+//         }
+//         return res;
 
         
+//     }
+// }
+class Solution {
+ public int[] minOperations(String boxes) {
+    int n=boxes.length();
+    //prefix sum from l-r
+    int  res[]=new int[n];
+    int ones=0;
+    int moves=0;
+    for(int i=0;i<n;i++){
+        res[i]+=moves;
+        ones+=boxes.charAt(i)=='1'?1:0;
+        moves+=ones;
     }
+    //prefix sum from r-l
+     ones=0;
+     moves=0;
+    for(int i=n-1;i>=0;i--){
+        res[i]+=moves;
+        ones+=boxes.charAt(i)=='1'?1:0;
+        moves+=ones;
+
+
+    }
+    return res;
+
+ }
 }
