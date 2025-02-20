741fea8bd3aed49a073d6b8628279154e75f3835
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-02-20 11:59:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (40.73%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,18 +1,28 @@
+// class Solution {
+//     public String findDifferentBinaryString(String[] nums) {
+//         List<String>res=new ArrayList<>();
+//         int n=nums.length;
+//         generate("",n,res);
+//         res.removeAll(Arrays.asList(nums));
+//         return res.get(0);
+        
+//     }
+//     void generate(String curr,int n,List<String>res){
+//         if(curr.length()==n){
+//             res.add(curr);
+//             return ;
+//         }
+//         generate(curr+"0",n,res);
+//         generate(curr+"1",n,res);
+//     }
+// }
+//Cantor's diagonalization
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        List<String>res=new ArrayList<>();
+        StringBuilder sb=new StringBuilder();
         int n=nums.length;
-        generate("",n,res);
-        res.removeAll(Arrays.asList(nums));
-        return res.get(0);
-        
-    }
-    void generate(String curr,int n,List<String>res){
-        if(curr.length()==n){
-            res.add(curr);
-            return ;
+        for(int i=0;i<n;i++){
+            sb.append(nums[i].charAt(i)=='0'?'1':'0');
         }
-        generate(curr+"0",n,res);
-        generate(curr+"1",n,res);
-    }
-}
+        return sb.toString();
+    }}
