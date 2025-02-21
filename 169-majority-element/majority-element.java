780bd7263d77814d4387c4d7d7631b92c8cda8af
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-02-21 17:07:26 +0530

    Time: 12 ms (36.07%) | Memory: 49.5 MB (85.38%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -9,22 +9,37 @@
 // }
 //Boyer-Moore Voting Algorithm:
 
-class Solution{
-    public int  majorityElement(int[] nums) {
-        int n=nums.length;
-        int c=0;
-        int v=0;
-        for(int num:nums){
-            if(c==0) v=num;
-            c+=(v==num)?1:-1;
+// class Solution{
+//     public int  majorityElement(int[] nums) {
+//         int n=nums.length;
+//         int c=0;
+//         int v=0;
+//         for(int num:nums){
+//             if(c==0) v=num;
+//             c+=(v==num)?1:-1;
 
             
-        }
-        c=0;
-        for(int num:nums){
-            if(v==num) c++;
-        }
-        return (c>=n/2)?v:-1;
-    }
+//         }
+//         c=0;
+//         for(int num:nums){
+//             if(v==num) c++;
+//         }
+//         return (c>=n/2)?v:-1;
+//     }
 
+// }
+class Solution{
+    public int  majorityElement(int[] nums) {
+        HashMap<Integer,Integer>mp=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+
+        }
+        for(Map.Entry<Integer,Integer>e:mp.entrySet()){
+            if(e.getValue()>nums.length/2){
+                return e.getKey();
+            }
+        }
+        return -1;
+    }
 }
