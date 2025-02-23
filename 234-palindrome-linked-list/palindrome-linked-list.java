672da65c07de30dc2a07d3984d5f219716d40006
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-02-23 17:04:38 +0530

    Time: 16 ms (8.17%) | Memory: 56.7 MB (89.17%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -22,3 +22,40 @@ class Solution {
         
     }
 }
+/* 
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null) return true;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next; 
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+        ListNode curr = slow;
+        ListNode next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr= next;
+        }
+
+        ListNode right = prev;
+        ListNode left= head;
+
+        while(right != null){
+            if(right.val != left.val){
+                return false;
+            }
+            right = right.next;
+            left = left.next;
+        }
+        return true;
+    }
+}*/
