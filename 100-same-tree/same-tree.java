56039d791b249fb30ea1ebd7fa29e49a44fe8531
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-02-27 17:03:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (11.63%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -13,17 +13,37 @@
  *     }
  * }
  */
+// class Solution {
+//     public boolean isSameTree(TreeNode p, TreeNode q) {
+//         if(p==null && q==null){
+//             return true;
+//         }
+//          if(p==null ||  q==null){
+//             return false;
+//         }
+//          if(p.val!=q.val){
+//             return false;
+//         }
+//         return isSameTree(p.left,q.left)&& isSameTree(p.right,q.right);     
+//     }
+// }
+//post-order and pre-order traversals preserve the structural properties of a binary tree, while in-order does not
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null){
-            return true;
-        }
-         if(p==null ||  q==null){
-            return false;
-        }
-         if(p.val!=q.val){
-            return false;
-        }
-        return isSameTree(p.left,q.left)&& isSameTree(p.right,q.right);     
+        ArrayList<Integer>a=new ArrayList<>();
+        ArrayList<Integer>b=new ArrayList<>();
+        preorder(b,p);
+        preorder(a,q);
+        return a.equals(b);
+    }
+    void preorder(ArrayList<Integer>a,TreeNode p){
+        if(p==null){
+            a.add(Integer.MIN_VALUE);
+
+        return ;
+        } 
+        a.add(p.val);
+        preorder(a,p.left);
+        preorder(a,p.right);
     }
 }
