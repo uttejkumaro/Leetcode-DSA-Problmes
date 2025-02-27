9aa904d84374a031d16347b481eccae37a1e39e6
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-02-27 18:46:40 +0530

    Time: 1 ms (72.22%) | Memory: 41.9 MB (90.46%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -13,40 +13,30 @@
  *     }
  * }
  */
- //use bfs to find each right node in each level
-
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        if (root == null) {
-            return res;
-        }
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            TreeNode rightMost = null;
-
-            for (int i = 0; i < size; i++) {
-                TreeNode curr = queue.poll();
-                rightMost = curr;
-
-                if (curr.left != null) {
-                    queue.add(curr.left);
+         List<Integer>res=new ArrayList<>();
+        if(root==null) return new ArrayList<>();
+        Queue<TreeNode>q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            TreeNode rightmost=null;
+            for(int i=0;i<size;i++){
+                TreeNode curr=q.poll();
+                rightmost=curr;
+                if(curr.left!=null){
+                    q.add(curr.left);
                 }
-                if (curr.right != null) {
-                    queue.add(curr.right);
+                if(curr.right!=null){
+                    q.add(curr.right);
                 }
             }
+            if(rightmost!=null) res.add(rightmost.val);
 
-            // After the loop, rightMost will be the last node of the current level
-            if (rightMost != null) {
-                res.add(rightMost.val);
-            }
+        
         }
-
         return res;
+        
     }
 }
