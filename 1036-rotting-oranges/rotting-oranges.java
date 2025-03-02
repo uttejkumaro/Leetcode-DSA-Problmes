0c78fd69d64f6a6d48d58393a6b7dd5844f9fea7
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-03-02 19:18:30 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (48.32%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,48 +1,43 @@
-//bfs -using minute counter to count no of step
 class Solution {
     public int orangesRotting(int[][] grid) {
-        if(grid==null || grid.length==0){
-            return  -1;
-        }
-        int m=grid.length;
-        int n=grid[0].length;
+        int n=grid.length;
+        int m=grid[0].length;
+        int freshOranges=0;
         Queue<int []>q=new LinkedList<>();
-        int fO=0;//freshoranges
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        int mins=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    //add index to queue
-                    q.offer(new int[]{i,j});
+                    q.add(new int[]{i,j});
                 }
                 else if(grid[i][j]==1){
-                    fO+=1;
+                    freshOranges+=1;
                 }
-            }
 
+            }
         }
-        int dirs[][]={{0,1},{1,0},{0,-1},{-1,0}};
-        int mins=0;
-        //bfs
-        while(!q.isEmpty() &&  fO>0){
+        int dirs[][]={{1,0},{-1,0},{0,1},{0,-1}};
+        while(!q.isEmpty() && freshOranges>0){
             int size=q.size();
             for(int i=0;i<size;i++){
-                int curr[]=q.poll();
-                int x=curr[0];
-                int y=curr[1];
-                for(int dir[]:dirs ){
-                    int x1=x+dir[0];
-                    int y1=y+dir[1];
-                
-                if(x1 >=0 && x1 < m && y1 >=0 && y1<n && grid[x1][y1]==1){
-                    grid[x1][y1]=2; //replace rot from fresh
-                    q.offer(new int []{x1,y1});
-                    fO-=1;
+            int []curr=q.poll();
+            int x,y;
+            x=curr[0];
+            y=curr[1];
+            for(int []dir:dirs){
+                int x1=x+dir[0];
+                int y1=y+dir[1];
+                if(x1>=0 && x1<n && y1>=0 && y1<m && grid[x1][y1]==1){
+                    grid[x1][y1]=2;
+                    q.add(new int []{x1,y1});
+                    freshOranges-=1;
                 }
-                }
-
             }
-            mins+=1;
+            }
+            mins+=1;//mins will incre after expore in 4 dirs;
+
         }
-        return fO==0?mins:-1;
+        return freshOranges==0?mins:-1;
+        
     }
 }
