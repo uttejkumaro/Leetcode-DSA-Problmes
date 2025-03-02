2dfc80497a169a0ff6617cf5958fd15921829692
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-03-02 20:28:51 +0530

    Time: 2 ms (46.64%) | Memory: 48.6 MB (8.62%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,23 +1,50 @@
+// class Solution {
+//     private void dfs(int[][] isConnected, boolean[] visited, int i) {
+//         visited[i] = true;
+//         for (int j = 0; j < isConnected.length; j++) {
+//             if (isConnected[i][j] == 1 && !visited[j]) {
+//                 dfs(isConnected, visited, j);
+//             }
+//         }
+//     }
+//     public int findCircleNum(int[][] isConnected) {
+//         int n= isConnected.length;
+//         boolean []visited=new boolean[n];
+//         int count=0;
+//         for(int i=0;i<n;i++){
+//             if(!visited[i]){
+//                 count++;
+//                 dfs(isConnected,visited,i);
+//             }
+//         }    
+//         return count;    
+//     }
+
+// }
 class Solution {
-    private void dfs(int[][] isConnected, boolean[] visited, int i) {
-        visited[i] = true;
-        for (int j = 0; j < isConnected.length; j++) {
-            if (isConnected[i][j] == 1 && !visited[j]) {
-                dfs(isConnected, visited, j);
+    public int findCircleNum(int[][] isConnected) {
+          int n= isConnected.length;
+        boolean []vis=new boolean[n];
+        int count=0;
+        Queue<Integer>q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                vis[i]=true;
+                q.add(i);
+                while(!q.isEmpty()){
+                    int city=q.poll();
+                    for(int j=0;j<n;j++){
+                        if(isConnected[city][j]==1 && !vis[j]){
+                            vis[j]=true;
+                            q.add(j);
+                        }
+                    }
+
+                }
+                            count++;
             }
         }
+        return count;
     }
-    public int findCircleNum(int[][] isConnected) {
-        int n= isConnected.length;
-        boolean []visited=new boolean[n];
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                count++;
-                dfs(isConnected,visited,i);
-            }
-        }    
-        return count;    
-    }
-
+  
 }
