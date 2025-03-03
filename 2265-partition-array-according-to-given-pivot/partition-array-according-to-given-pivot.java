1758875f9b335e9623930418cda5828ceee6de33
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-03-03 08:10:14 +0530

    Time: 5 ms (85.39%) | Memory: 68.2 MB (35.78%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,26 +1,43 @@
-class Solution {
-    public int[] pivotArray(int[] nums, int pivot) {
-        // int n=nums.length;
-        // int[] res=new int[n];
-       ArrayList<Integer>before=new ArrayList<>();
-       ArrayList<Integer>equal=new ArrayList<>();
-       ArrayList<Integer>after=new ArrayList<>();
-       for(int i:nums){
-        if(i<pivot) before.add(i);
-        else if(i>pivot)after.add(i);
-        else equal.add(i);
-       }
-       int idx=0;
-       for(int i:before){
-        nums[idx++]=i;
-       }
-       for(int i:equal){
-        nums[idx++]=i;
-       }for(int i:after){
-        nums[idx++]=i;
-       }
-        return nums;
+// class Solution {
+//     public int[] pivotArray(int[] nums, int pivot) {
+//         // int n=nums.length;
+//         // int[] res=new int[n];
+//        ArrayList<Integer>before=new ArrayList<>();
+//        ArrayList<Integer>equal=new ArrayList<>();
+//        ArrayList<Integer>after=new ArrayList<>();
+//        for(int i:nums){
+//         if(i<pivot) before.add(i);
+//         else if(i>pivot)after.add(i);
+//         else equal.add(i);
+//        }
+//        int idx=0;
+//        for(int i:before){
+//         nums[idx++]=i;
+//        }
+//        for(int i:equal){
+//         nums[idx++]=i;
+//        }for(int i:after){
+//         nums[idx++]=i;
+//        }
+//         return nums;
       
         
+//     }
+// }
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n=nums.length;
+        int[] res=new int[n];
+        int idx=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]<pivot) res[idx++]=nums[i];            
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]==pivot) res[idx++]=nums[i];            
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>pivot) res[idx++]=nums[i];            
+        }
+        return res;
     }
 }
