0b23358fe03765255c6c50d64144cbc3da239841
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-03-07 10:23:49 +0530

    Time: 83 ms (13.65%) | Memory: 44.6 MB (37.65%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -8,29 +8,66 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         if(lists==null) return null;
+//         PriorityQueue<ListNode>pq=new PriorityQueue<>((a,b)->(a.val-b.val));
+//         for(ListNode list:lists){
+//             if(list!=null){
+//                 pq.add(list);
+//             }
+//         }
+
+//         ListNode dummy=new ListNode();
+//         ListNode curr=dummy;
+//             while(!pq.isEmpty()){
+//                ListNode smallest =pq.poll();
+//                curr.next=smallest;
+//                curr=curr.next;
+//               if(smallest.next!=null){
+//                 pq.add(smallest.next);
+//               }
+
+
+//             }
+//             return dummy.next;       
+        
+//     }
+// }
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        if(lists==null) return null;
-        PriorityQueue<ListNode>pq=new PriorityQueue<>((a,b)->(a.val-b.val));
-        for(ListNode list:lists){
-            if(list!=null){
-                pq.add(list);
-            }
-        }
-
-        ListNode dummy=new ListNode();
+    ListNode merge(ListNode list1,ListNode list2){
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+        ListNode dummy=new ListNode(0);
         ListNode curr=dummy;
-            while(!pq.isEmpty()){
-               ListNode smallest =pq.poll();
-               curr.next=smallest;
-               curr=curr.next;
-              if(smallest.next!=null){
-                pq.add(smallest.next);
-              }
-
-
+        ListNode head1=list1;
+        ListNode head2=list2;
+        while(head1!=null && head2!=null){
+            if(head1.val<head2.val){
+                curr.next=head1;
+                head1=head1.next;
             }
-            return dummy.next;       
+            else{
+               curr.next=head2;
+                head2=head2.next;
+            }
+            curr=curr.next;
+        }
+        if(head1!=null){
+           curr.next=head1;
+        }
+        if(head2!=null){
+           curr.next=head2;
+        }
+        return dummy.next;
+
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+     ListNode res=null;
+     for(int i=0;i<lists.length;i++){
+        res=merge(res,lists[i]);
+     }
+     return res;
         
     }
 }
