e3b870f1c008aab4be5c9f10cfc8c0ef4d68a55a
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-03-07 15:12:43 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (9.85%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,39 +1,69 @@
-class MyQueue {
-    Stack<Integer> s1;
-    Stack<Integer> s2;
+// class MyQueue {
+//     Stack<Integer> s1;
+//     Stack<Integer> s2;
 
-    public MyQueue() {
-    s1=new Stack<>();
-    s2=new Stack<>();
+//     public MyQueue() {
+//     s1=new Stack<>();
+//     s2=new Stack<>();
 
         
-    }
+//     }
     
-    public void push(int x) {
+//     public void push(int x) {
         
-        while(!s1.isEmpty()){
+//         while(!s1.isEmpty()){
+//                 s2.push(s1.pop());
+//             }
+//                s2.push(x);            
+//         while(!s2.isEmpty()){
+//             s1.push(s2.pop());
+//         }
+        
+//     }
+    
+//    int pop() {
+//         int x=s1.pop();
+//         return x;
+        
+//     }
+    
+//     public int peek() {
+//         return s1.peek();
+        
+//     }
+    
+//     public boolean empty() {
+//         return s1.isEmpty();
+//     }
+// }
+class MyQueue {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    public void push(int x) {
+        s1.push(x);
+    }
+
+    public int pop() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
                 s2.push(s1.pop());
             }
-               s2.push(x);            
-        while(!s2.isEmpty()){
-            s1.push(s2.pop());
         }
-        
+        return s2.pop();
     }
-    
-   int pop() {
-        int x=s1.pop();
-        return x;
-        
-    }
-    
+
     public int peek() {
-        return s1.peek();
-        
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.peek();
     }
-    
+
     public boolean empty() {
-        return s1.isEmpty();
+        return s1.isEmpty() && s2.isEmpty();
     }
 }
 
