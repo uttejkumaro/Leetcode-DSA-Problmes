30b1b78dca953b7fc5a01e7780c4ea3b298c9aad
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-03-12 17:44:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (8.64%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -18,16 +18,34 @@
 //         return prev1;
 //     }
 // }
+// class Solution{
+//     public int rob(int nums[]){
+//         int n=nums.length;
+//         if(n==1) return nums[0];
+//         int dp[]=new int[n+1];
+//         dp[0]=nums[0];
+//         dp[1]=Math.max(nums[0], nums[1]);
+//         for(int i=2;i<n;i++){
+//             dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
+//         }
+//         return dp[n-1];
+//     }
+// }
 class Solution{
     public int rob(int nums[]){
         int n=nums.length;
-        if(n==1) return nums[0];
-        int dp[]=new int[n+1];
-        dp[0]=nums[0];
-        dp[1]=Math.max(nums[0], nums[1]);
-        for(int i=2;i<n;i++){
-            dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
-        }
-        return dp[n-1];
+        int dp[]=new int[n];
+        Arrays.fill(dp,-1);
+        return helper(nums,n-1,dp);
+    }
+    int helper(int nums[],int n,int dp[]){
+        if(n<0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        int skip=helper(nums,n-1,dp);
+        int include=helper(nums,n-2,dp)+nums[n];
+        dp[n]=Math.max(skip,include);
+        return dp[n];
+
+        
     }
 }
