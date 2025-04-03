6dc1c6a2a70a777d1490ba14b91d120e0bc068a8
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-04-04 05:20:37 +0530

    Time: 1 ms (52.77%) | Memory: 41.5 MB (82.54%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,21 +1,39 @@
+// class Solution{
+
+//     public int minAddToMakeValid(String s){
+//         int openBrackets = 0;
+//         int minAdds= 0;
+//         for(char c:s.toCharArray()){
+//             if(c == '(') {
+//                 openBrackets++;
+//             }else {
+//                 if(openBrackets>0){
+//                     openBrackets--;
+//                 }
+//                 else {
+//                     minAdds++;
+//                 }
+//             }
+//         }
+
+//         return minAdds+openBrackets;
+//     }
+// }
 class Solution{
-
     public int minAddToMakeValid(String s){
-        int openBrackets = 0;
-        int minAdds= 0;
-        for(char c:s.toCharArray()){
-            if(c == '(') {
-                openBrackets++;
-            }else {
-                if(openBrackets>0){
-                    openBrackets--;
-                }
-                else {
-                    minAdds++;
-                }
+        Stack<Character>st=new Stack<>();
+        for (char c:s.toCharArray()){
+            
+            if(!st.isEmpty() && (c==')' && st.peek()=='(')){
+                st.pop();
             }
-        }
+            else{
+                st.push(c);
+            }
 
-        return minAdds+openBrackets;
+        }
+        return st.size();
     }
 }
+
+        
