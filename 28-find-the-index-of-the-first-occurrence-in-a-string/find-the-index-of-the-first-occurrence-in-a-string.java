85634f8abcb34cdf0b269f98e5f365cb91b6755f
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-04-04 08:12:18 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (99.10%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,25 +1,35 @@
 //naive searching algo
+//https://www.geeksforgeeks.org/naive-algorithm-for-pattern-searching/?ref=lbp
+//https://www.geeksforgeeks.org/rabin-karp-algorithm-for-pattern-searching/?ref=lbp
+// class Solution {
+//     public int strStr(String haystack, String needle) {
+//         int n=haystack.length();
+//         int m=needle.length();
+//         int res=Integer.MAX_VALUE;
+//         for(int i=0;i<=n-m;i++){
+//             int j;
+//             for( j=0;j<m;j++){
+//                 if(haystack.charAt(i+j)!=needle.charAt(j)){
+//                     break;
+//                 }
+//             }
+//             if(j==m){
+//                 int ans=i;
+//                  res=Math.min(res,ans);
+//             }     
+            
+           
+//         }
+//         return (res!=Integer.MAX_VALUE) ?res:-1;
+        
+//     }
+// }
 class Solution {
     public int strStr(String haystack, String needle) {
         int n=haystack.length();
         int m=needle.length();
         int res=Integer.MAX_VALUE;
-        for(int i=0;i<=n-m;i++){
-            int j;
-            for( j=0;j<m;j++){
-                if(haystack.charAt(i+j)!=needle.charAt(j)){
-                    break;
-                }
-            }
-            if(j==m){
-                int ans=i;
-                 res=Math.min(res,ans);
-            }
-            
-            
-           
-        }
-        return (res!=Integer.MAX_VALUE) ?res:-1;
-        
+        if(haystack.indexOf(needle)>=0) return haystack.indexOf(needle) ;
+        return -1;
     }
 }
