62ff03bd4c5b2076638d994be356d8472ce86bf8
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-04-06 15:49:48 +0530

    Time: 24 ms (41.01%) | Memory: 42.5 MB (73.43%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,27 +1,29 @@
 class Solution {
-    public int minCost(int n, int[] cuts) {
-        int total []=new int[cuts.length+2];
-        total[0]=0;
-        total[total.length-1]=n;
-        for(int i=0;i<cuts.length;i++){
-            total[i+1]=cuts[i];
-        }
-        Arrays.sort(total);
-        int memo[][]=new int[total.length][total.length];
-        return helper(total,0,total.length-1,memo);                
-    }
-    int helper(int cuts[],int left,int right,int memo[][]){
-        if(left+1==right){
-            return 0;
-        }
-        if(memo[left][right]!=0){
-            return memo[left][right];
-        }
+    int helper(int i,int j,int n,int dp[][],int cuts[]){
+        if(i>j) return 0;
         int min=Integer.MAX_VALUE;
-        for(int i=left+1;i<right;i++){
-            int cost=cuts[right]-cuts[left]+helper(cuts,left,i,memo)+helper(cuts,i,right,memo);
-            min=Math.min(min,cost);
+        if(dp[i][j]!=-1) return dp[i][j];
+        for(int idx=i;idx<=j;idx++){
+           int  cost=cuts[j+1]-cuts[i-1]+helper(idx+1,j,n,dp,cuts)+helper(i,idx-1,n,dp,cuts);
+           min=Math.min(cost,min);
         }
-        return memo[left][right] =min;
+        return dp[i][j]=min;
+    }
+    public int minCost(int n, int[] cuts) {
+        int m=cuts.length;
+        int newCuts[]=new int[cuts.length+2];
+        newCuts[0]=0;
+        newCuts[m+1]=n;
+        for(int i=1;i<newCuts.length-1;i++){
+            newCuts[i]=cuts[i-1];
+        }
+        Arrays.sort(newCuts);
+        int dp[][]=new int [m+2][m+2];
+        for(int row[]:dp){
+            Arrays.fill(row,-1);
+        }
+
+        return helper(1,m,n,dp,newCuts);
+        
     }
 }
