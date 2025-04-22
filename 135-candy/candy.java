183677cbe05b8cd6f9fcdb13707cdc0955164d54
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-04-23 03:42:21 +0530

    Time: 4 ms (26.89%) | Memory: 46.3 MB (10.45%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int left[]=new int[n];  
+        
+        Arrays.fill(left,1);   
+        for(int i=1;i<n;i++){
+            if(ratings[i-1]<ratings[i]){
+                left[i]=left[i-1]+1;
+            }
+        }
+        int total = left[n - 1];
+        int right = 1;
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                right++;
+            }
+            else{
+                right=1;
+            }
+        left[i]=Math.max(left[i],right);
+        total+=left[i];
+        }        
+        return total;        
+    }
+}
