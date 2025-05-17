91212a04afd4a0abf654378b5acbfccefcf92ced
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-05-17 10:25:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (30.03%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,51 +1,18 @@
-/* time complexity:O(nlogn)
- space  complexity:O(1)
-inplace,stable:y,y
 class Solution {
-    public int[] sortColors(int[] nums) {
-        Arrays.sort(nums);
-        return nums;
+    public void sortColors(int[] nums) {
+        int n=nums.length;
+        int count0=0;
+        int count1=0;
+        int count2=0;
+        for(int i:nums){
+            if(i==0) count0++;
+            else if(i==1) count1++;
+            else count2++;
+        }
+        for(int i=0;i<count0;i++){
+            nums[i]=0;
+        }
+        for(int i=count0;i<count0+count1;i++) nums[i]=1;
+        for(int i=count0+count1;i<n;i++) nums[i]=2;
     }
 }
-*/
-//dutch national flag  algorithm
-//O(n),O(1)
-class Solution{
-      public void  sortColors(int[] nums) {
-        int low=0;int mid=0;int high=nums.length-1;
-        while(mid<=high){
-            if(nums[mid]==0){
-                //swap nums[low],nums[mid],increment low & mid
-                int temp=nums[low];
-                nums[low]=nums[mid];
-                nums[mid]=temp;
-                mid++;
-                low++;
-            }
-            else if(nums[mid]==1){
-                mid++;
-            }
-            else{
-                int temp=nums[mid];
-                nums[mid]=nums[high];
-                nums[high]=temp;
-                high--;
-            }
-
-        }
-
-
-
-
-
-
-
-
-      }
-
-
-
-
-
-}
-
