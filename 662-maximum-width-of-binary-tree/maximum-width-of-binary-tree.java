55467359ea49763b1fc1a68723d41ab4777e4999
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-05-24 11:50:42 +0530

    Time: 2 ms (38.71%) | Memory: 43.6 MB (44.69%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -1,54 +1,36 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
- class pair<k,v>{
-    k key;
-    v value;
-    pair(k key,v value ){
-        this.key=key;
-        this.value=value;
-    }
-        k getKey(){return key;}
-        v getValue() {
-        return value;
-        }
- }
 class Solution {
-    public int widthOfBinaryTree(TreeNode root) {
-        if(root==null) return 0;
-        Queue<pair<TreeNode,Integer>>q=new LinkedList<>();
-        q.add(new pair<>(root,0));
-        int max=0;
-        while(!q.isEmpty()){
-           int size=q.size();
-             int left=0;
-            int right=0;
-           for(int i=0;i<size;i++){
-            
-           pair<TreeNode, Integer> pair = q.poll();
-                TreeNode node = pair.getKey();
-                int pos = pair.getValue();
-            if(i==0) left=pos;
-            if(i==size-1) right=pos;
-            if(node.left!=null) q.add(new pair<>( node.left,2*pos));
-            if(node.right!=null) q.add(new pair<>(node.right,2*pos+1));
-           }
-            max=Math.max(max,right-left+1);
+    class NodeInfo {
+        TreeNode node;
+        int index;
+        NodeInfo(TreeNode node, int index) {
+            this.node = node;
+            this.index = index;
         }
-        return max;
-        
-    
+    }
+
+    public int widthOfBinaryTree(TreeNode root) {
+        if (root == null) return 0;
+        Queue<NodeInfo> q = new LinkedList<>();
+        q.offer(new NodeInfo(root, 0));
+        int maxWidth = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            int minIndex = q.peek().index;
+            int first = 0, last = 0;
+            for (int i = 0; i < size; i++) {
+                NodeInfo current = q.poll();
+                int index = current.index - minIndex;
+                if (i == 0) first = index;
+                if (i == size - 1) last = index;
+                if (current.node.left != null)
+                    q.offer(new NodeInfo(current.node.left, 2 * index));
+                if (current.node.right != null)
+                    q.offer(new NodeInfo(current.node.right, 2 * index + 1));
+            }
+            maxWidth = Math.max(maxWidth, last - first + 1);
+        }
+
+        return maxWidth;
     }
 }
