0fc8eb631468dc58c92cf26ae7148c88ba301350
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-06-03 14:48:50 +0530

    Time: 31 ms (58.12%) | Memory: 51.6 MB (50.77%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -50,3 +50,22 @@ class Solution {
         return result;
     }
 }
+
+//     Set<List<Integer>> set = new HashSet<>();
+//     int n = nums.length;
+
+//     for (int i = 0; i < n - 2; i++) {
+//         for (int j = i + 1; j < n - 1; j++) {
+//             for (int k = j + 1; k < n; k++) {
+//                 if (nums[i] + nums[j] + nums[k] == 0) {
+//                     List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+//                     Collections.sort(triplet);
+//                     set.add(triplet);
+//                 }
+//             }
+//         }
+//     }
+
+//     return new ArrayList<>(set);
+// }
+// }
