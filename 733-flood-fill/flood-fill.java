8ea2ffaebd458f93e67ce5c7b4824a4300cdf068
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-07-06 16:20:59 +0530

    Time: 1 ms (47.15%) | Memory: 45.2 MB (33.25%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,39 +1,29 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n = image.length;        
-        int m = image[0].length;     
-        int originalColor = image[sr][sc];
-        if (originalColor == color) {
-            return image;
-        }
-        
-        bfs(image, sr, sc, color, originalColor, n, m);
+        int originalColor=image[sr][sc];
+        if(originalColor==color)return image;
+        bfs(image,sr,sc,color,originalColor);
         return image;
     }
-
-    private void bfs(int[][] image, int r, int c, int color, int originalColor, int n, int m) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{r, c});
-        image[r][c] = color;
-        
-        // Directions for moving in the grid (Right, Down, Left, Up)
-        int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
-        
-        while (!queue.isEmpty()) {
-            int[] pixel = queue.poll();
-            int x = pixel[0];
-            int y = pixel[1];
-            
-            for (int dir[]:directions) {
-
-                int newRow = x + dir[0];
-                int newCol = y + dir[1];
-                
-                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && image[newRow][newCol] == originalColor) {
-                    image[newRow][newCol] = color;
-                    queue.offer(new int[]{newRow, newCol});
+        void bfs(int[][] image, int sr, int sc, int color,int originalColor){
+        int dirs[][]={{1,0},{-1,0},{0,1},{0,-1}};
+        int n=image.length;
+        int m=image[0].length;
+        Queue<int []>q=new LinkedList<>();
+        q.add(new int []{sr,sc});
+        image[sr][sc]=color;
+        while(!q.isEmpty()){
+            int curr[]=q.poll();
+            int x=curr[0];
+            int y=curr[1];
+            for(int dir[]:dirs){
+                int newX=x+dir[0];
+                int newY=y+dir[1];
+                if(newX<n && newY<m && newX>=0 && newY>=0 && image[newX][newY]==originalColor){ image[newX][newY]=color;
+                    q.add(new int[]{newX,newY});
                 }
             }
         }
+        
     }
 }
