eda761a2df12263e63b7b8bf26e7a8efe0f3c0f0
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-07-26 16:25:58 +0530

    Time: 487 ms (51.20%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,16 @@
+SELECT 
+    p.product_id,
+    COALESCE(latest.new_price, 10) AS price
+FROM 
+    (SELECT DISTINCT product_id FROM Products) p
+LEFT JOIN (
+    SELECT product_id, new_price
+    FROM Products
+    WHERE (product_id, change_date) IN (
+        SELECT product_id, MAX(change_date)
+        FROM Products
+        WHERE change_date <= '2019-08-16'
+        GROUP BY product_id
+    )
+) latest
+ON p.product_id = latest.product_id;
