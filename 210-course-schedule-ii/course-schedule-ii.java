6ac1882cc341a0bb4f24cf14ba8063b1c1a1e8ca
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-07-27 15:29:26 +0530

    Time: 4 ms (91.98%) | Memory: 46.6 MB (7.39%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,43 +1,33 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int n=prerequisites.length;
-        int idx=0;
-        Set<Integer>set=new HashSet<>();
-        int[] courseOrder=new int[numCourses];
-        int inDegree[]=new int[numCourses];
-        List<List<Integer>>graph=new ArrayList<>();
+        ArrayList<ArrayList<Integer>>al=new ArrayList<>();
+        //int n=prerequisites.length;
+        int inOrder[]=new int[numCourses];
         for(int i=0;i<numCourses;i++){
-            graph.add(new ArrayList<>());
-        } 
-        for( int preq[]:prerequisites){
-            graph.get(preq[1]).add(preq[0]);
-            inDegree[preq[0]]++;
-
+            al.add(new ArrayList<>());
         }
+        for(int preq[]:prerequisites){
+            int u=preq[0];
+            int v=preq[1];
+            al.get(v).add(u);
+            inOrder[u]++;
+        }
+        Set<Integer>st=new HashSet<>();
         Queue<Integer>q=new LinkedList<>();
         for(int i=0;i<numCourses;i++){
-            if(inDegree[i]==0){
-                q.add(i);
-            }
-            
+            if(inOrder[i]==0)q.add(i);
         }
-        int count=0;
+        int[] res=new int[numCourses];
+        int idx=0;
         while(!q.isEmpty()){
             int curr=q.poll();
-            courseOrder[idx++] = curr;
-           
-            count++;
-            for(int neigh:graph.get(curr)){
-                inDegree[neigh]--;
-                if(inDegree[neigh]==0){
-                    q.add(neigh);
-                }
+            res[idx++]=curr;
+            for(int neigh:al.get(curr)){
+                inOrder[neigh]--;
+                if(inOrder[neigh]==0)q.add(neigh);
+
             }
         }
-       
-        return count==numCourses?courseOrder:new int[]{};
-
-        
-        
+        return idx==numCourses?res:new int[]{};        
     }
 }
