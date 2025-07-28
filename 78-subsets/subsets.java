8a00dfa60cc0ade72c4fef0464065cf163d42ffc
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-07-28 21:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (30.03%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -17,3 +17,26 @@ class Solution {
         helper(res,i+1,curr,nums);
     }
 }
+/**
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        int total = 1 << n;  // 2^n subsets
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int mask = 0; mask < total; mask++) {
+            List<Integer> subset = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if ((mask & (1 << j)) != 0) {
+                    subset.add(nums[j]);
+                }
+            }
+            result.add(subset);
+        }
+
+        return result;
+    }
+}
+ */
