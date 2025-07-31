aa23709d9aee72d7323f6003a495b917cd81ca05
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-07-31 19:16:41 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (78.59%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,29 +1,26 @@
 class Solution {
-
-    private void helper(int index, String digits, int n, String[] store, StringBuilder path, List<String> result) {
-        if (index == n) {
-            if (path.toString().length() > 0)
-                result.add(path.toString());
-            return;
-        }
-        char currentChar = digits.charAt(index);
-        int currentDigit = currentChar - '0';
-
-        for (char ch: store[currentDigit].toCharArray()) {
-            path.append(ch);
-            helper(index + 1, digits, n, store, path, result);
-            path.deleteCharAt(path.length() - 1);
-        }
-        
-    }
-
     public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
-        StringBuilder path = new StringBuilder();
-        
-        String[] store = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
-        helper(0, digits, digits.length(), store, path, result);
+        int n=digits.length();
+        ArrayList<String>result=new ArrayList<>();
+        StringBuilder path =new StringBuilder();
+        String store[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        helper(digits,n,result,path,store,0);
         return result;
+    }
+    void  helper(String digits,int n,ArrayList res,StringBuilder path,String store[],int idx){
+        if(idx==n){
+            if(path.toString().length()>0){
+                res.add(path.toString());
+            }
+                return;
+        }
+        int currDigit=digits.charAt(idx)-'0';
+        for(char c:store[currDigit].toCharArray()){
+            path.append(c);
+            helper(digits,n,res,path,store,idx+1);
+            path.deleteCharAt(path.length()-1);
+
+        }
+
     }
 }
