7d200e58a1e236c8cd6144e337165d02a3192785
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-08-15 13:07:49 +0530

    Time: 1 ms (86.72%) | Memory: 45.4 MB (95.66%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -73,3 +73,37 @@ class Solution {
         return count;
     }
 }
+/**
+
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                bfs(isConnected, visited, i);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private void bfs(int[][] isConnected, boolean[] visited, int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            for (int neighbor = 0; neighbor < isConnected.length; neighbor++) {
+                if (isConnected[node][neighbor] == 1 && !visited[neighbor]) {
+                    queue.offer(neighbor);
+                    visited[neighbor] = true;
+                }
+            }
+        }
+    }
+}
+ */
