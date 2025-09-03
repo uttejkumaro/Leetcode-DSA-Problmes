e7106c35c6f7267456fd372df5b48fd3022b1c1b
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-09-03 13:28:33 +0530

    Time: 25 ms (39.93%) | Memory: 45.8 MB (53.02%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0;i<tasks.length;i++){
             count[tasks[i]-'A']++;
         }
-        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());//max heap 
         for(int c:count){
             if(c>0){
             pq.add(c);
@@ -34,3 +34,25 @@ class Solution {
         
     }
 }
+/**Why time vs n+1?
+
+Case 1: pq.isEmpty() (all tasks done)
+
+Example: In the last round, suppose only 2 tasks remain.
+
+We only need the actual number of tasks executed (time).
+
+No need to pad with idle slots because we’re finished.
+
+✅ Example: n=2, last cycle has just A and B.
+We execute 2 tasks → only 2 intervals needed (not 3).
+
+Case 2: !pq.isEmpty() (more tasks left)
+
+We must assume a full cycle of n+1 intervals (tasks + possible idle).
+
+This ensures that the next same task has waited n intervals before reuse.
+
+Even if some slots are idle, they still count toward time.
+
+✅ Example: n=2, still tasks left, we process A, B → that cycle costs 3 intervals (n+1). */
