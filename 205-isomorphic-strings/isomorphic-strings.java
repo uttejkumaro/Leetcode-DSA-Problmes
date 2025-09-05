0c583fdf5f6e98e4822d791a9b55c3e461582e2f
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-09-05 20:10:28 +0530

    Time: 6 ms (89.37%) | Memory: 42.5 MB (52.86%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,34 +1,34 @@
-import java.util.HashMap;
+// import java.util.HashMap;
 
-class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        if(s.length() != t.length()) {
-            return false;
-        }
+// class Solution {
+//     public boolean isIsomorphic(String s, String t) {
+//         if(s.length() != t.length()) {
+//             return false;
+//         }
         
-        HashMap<Character, Character> charMap = new HashMap<>();
+//         HashMap<Character, Character> charMap = new HashMap<>();
         
-        for(int i = 0; i < s.length(); i++) {
-            char original = s.charAt(i);
-            char replace = t.charAt(i);
+//         for(int i = 0; i < s.length(); i++) {
+//             char original = s.charAt(i);
+//             char replace = t.charAt(i);
             
-            if(!charMap.containsKey(original)) {
-                if(charMap.containsValue(replace)) {
-                    return false;
-                }
-                charMap.put(original, replace);
-            } else {
-                char mappedChar = charMap.get(original);
-                if(mappedChar != replace) {
-                    return false;
-                }
-            }
-        }
+//             if(!charMap.containsKey(original)) {
+//                 if(charMap.containsValue(replace)) {
+//                     return false;
+//                 }
+//                 charMap.put(original, replace);
+//             } else {
+//                 char mappedChar = charMap.get(original);
+//                 if(mappedChar != replace) {
+//                     return false;
+//                 }
+//             }
+//         }
         
-        return true;
-    }
-}
-/**
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         if(s.length() != t.length()) return false;
@@ -50,4 +50,36 @@ class Solution {
         return true;
     }
 }
+ /**
+ Yes, using mapS[char] = i + 1 effectively stores the index at which the character was seen, with a small offset.
+ import java.util.HashMap;
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> mapS = new HashMap<>();
+        HashMap<Character, Integer> mapT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char cs = s.charAt(i);
+            char ct = t.charAt(i);
+
+            // Get last seen index (default 0 if not seen before)
+            int lastS = mapS.getOrDefault(cs, 0);
+            int lastT = mapT.getOrDefault(ct, 0);
+
+            // If mapping is inconsistent, not isomorphic
+            if (lastS != lastT) return false;
+
+            // Update last seen index (+1 to avoid confusion with default 0)
+            mapS.put(cs, i + 1);
+            mapT.put(ct, i + 1);
+        }
+
+        return true;
+    }
+}
+
+
  */
