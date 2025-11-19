7c809b61d78e2a591e09a449be924b1be1e15991
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2025-11-19 10:46:20 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.94%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.py
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.py
@@ -1,8 +1,12 @@
 class Solution:
     def findFinalValue(self, nums: List[int], original: int) -> int:
-        s=set(nums)
+        # s=set(nums)
+        # num=original
+        # while num in s:
+        #     num=num<<1
+        # return num
         num=original
-        while num in s:
+        while num in nums:
             num=num<<1
         return num
         
