280fa70adf0606a2b2bb7b587cd43b26d52d9bbf
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2026-05-26 19:08:11 +0530

    Time: 1 ms (100.00%) | Memory: 43.5 MB (89.90%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -1,17 +1,39 @@
+// class Solution {
+//     public int numberOfSpecialChars(String word) {
+//         Set<Character> st = new HashSet<>();
+
+//         for (char c : word.toCharArray()) {
+//             st.add(c);
+//         }
+
+//         int count = 0;
+
+//         for (char c = 'a'; c <= 'z'; c++) {
+//             if (st.contains(c) && st.contains((char)(c - 32))) {
+//                 count++;
+//             }
+//         }
+
+//         return count;
+//     }
+// }
 class Solution {
     public int numberOfSpecialChars(String word) {
-        Set<Character> st = new HashSet<>();
+        boolean[] lower = new boolean[26];
+        boolean[] upper = new boolean[26];
 
-        for (char c : word.toCharArray()) {
-            st.add(c);
+        for(char c : word.toCharArray()) {
+            if(c >= 'a' && c <= 'z')
+                lower[c - 'a'] = true;
+            else
+                upper[c - 'A'] = true;
         }
 
         int count = 0;
 
-        for (char c = 'a'; c <= 'z'; c++) {
-            if (st.contains(c) && st.contains((char)(c - 32))) {
+        for(int i = 0; i < 26; i++) {
+            if(lower[i] && upper[i])
                 count++;
-            }
         }
 
         return count;
