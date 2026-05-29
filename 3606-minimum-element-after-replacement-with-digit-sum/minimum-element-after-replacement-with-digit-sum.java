dc90d1673958b15f8cfc54c20e9be46af0e36ab4
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2026-05-29 07:42:30 +0530

    Time: 1 ms (100.00%) | Memory: 45 MB (27.04%) - LeetSync

--- 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.java
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.java
@@ -1,22 +1,44 @@
+// class Solution {
+//     public int minElement(int[] nums) {
+//         int n=nums.length;
+//         int res[]=new int[n];
+//         for(int i=0;i<n;i++){
+//             res[i]=sumOfDigits(nums[i]);
+//         }
+//         int min=Integer.MAX_VALUE;
+//          for(int i=0;i<n;i++){
+//             min=Math.min(min,res[i]);
+//         }
+//         return min;
+//     } 
+//     int sumOfDigits(int num){
+//         int sum=0;
+//         while(num>0){
+//             sum+=num%10;
+//             num/=10;
+//         }
+//         return sum;
+//     }
+// }
 class Solution {
     public int minElement(int[] nums) {
-        int n=nums.length;
-        int res[]=new int[n];
-        for(int i=0;i<n;i++){
-            res[i]=sumOfDigits(nums[i]);
+
+        int min = Integer.MAX_VALUE;
+
+        for(int num : nums) {
+
+            int sum = 0;
+
+            while(num > 0) {
+
+                int rem = num % 10;
+                sum = sum + rem;
+                num = num / 10;
+            }
+
+            min = Math.min(sum, min);
         }
-        int min=Integer.MAX_VALUE;
-         for(int i=0;i<n;i++){
-            min=Math.min(min,res[i]);
-        }
+
         return min;
-    } 
-    int sumOfDigits(int num){
-        int sum=0;
-        while(num>0){
-            sum+=num%10;
-            num/=10;
-        }
-        return sum;
     }
 }
