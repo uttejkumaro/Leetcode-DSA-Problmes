6831e02c02e153f259bb21dd0011f78d93dbf47a
o uttej kumar <117030083+uttejkumaro@users.noreply.github.com>
2026-06-06 16:18:47 +0530

    Time: 2 ms (98.84%) | Memory: 46.8 MB (13.15%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
@@ -1,21 +1,48 @@
+// class Solution {
+//     public int[] leftRightDifference(int[] nums) {
+//         int n=nums.length;
+//         int res[]=new int[n];
+//         int left[]=new int[n];
+//         int right[]=new int[n];
+//         int leftsum=0;
+//         int rightsum=0;
+//         for(int i=0;i<n;i++){
+//             left[i]=leftsum;
+//             leftsum+=nums[i];
+//         }
+//         for(int i=n-1;i>=0;i--){
+//             right[i]=rightsum;
+//             rightsum+=nums[i];
+//             res[i]=Math.abs(right[i]-left[i]);
+//         }
+//         return res;
+
+//     }
+// }
 class Solution {
     public int[] leftRightDifference(int[] nums) {
-        int n=nums.length;
-        int res[]=new int[n];
-        int left[]=new int[n];
-        int right[]=new int[n];
-        int leftsum=0;
-        int rightsum=0;
-        for(int i=0;i<n;i++){
-            left[i]=leftsum;
-            leftsum+=nums[i];
+        int leftSum = 0;
+        int rightSum = 0;
+        
+        // Calculate the total sum of the array first
+        for (int num : nums) {
+            rightSum += num;
         }
-        for(int i=n-1;i>=0;i--){
-            right[i]=rightsum;
-            rightsum+=nums[i];
-            res[i]=Math.abs(right[i]-left[i]);
+        
+        int[] ans = new int[nums.length];
+        
+        // Compute differences on the fly
+        for (int i = 0; i < nums.length; i++) {
+            // Exclude the current element from the right side sum
+            rightSum -= nums[i]; 
+            
+            // Calculate absolute difference
+            ans[i] = Math.abs(leftSum - rightSum); 
+            
+            // Include the current element into the left side sum for the next index
+            leftSum += nums[i]; 
         }
-        return res;
-
+        
+        return ans;
     }
 }
